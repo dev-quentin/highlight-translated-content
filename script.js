13830fe632d429d491738f2d1336d9398d49c33f
Quentin <53433390+dev-quentin@users.noreply.github.com>
2024-05-08 16:40:28 +0200

    Add: detect URL changes

--- script.js
+++ script.js
@@ -1,51 +1,28 @@
 // ==UserScript==
 // @name        Highlight translated Content
 // @namespace   Violentmonkey Scripts
-// @match       https://www.crunchyroll.com/*/videos/popular
+// @match       https://www.crunchyroll.com/*
 // @grant       none
 // @version     1.0
 // @author      dev-quentin
 // ==/UserScript==
 
-// Default locale and translations
+// Declaration of constants
 const defaultLocale = "n/a";
-const locales = [
-    "n/a", "hi-IN", "ta-IN", "pt-BR", "es-419", "de-DE", "te-IN", "en-US", "es-ES", "ja-JP", "ko-KR", "it-IT", "fr-FR",
-    "ms-MY", "ar-SA", "id-ID", "th-TH", "ru-RU", "vi-VN"
-];
-const translations = {
-    "n/a": "All", "hi-IN": "हिंदी", "ta-IN": "தமிழ்", "pt-BR": "Português (Brasil)", "es-419": "Espanol (América Latina)", "de-DE": "Deutsch",
-    "te-IN": "తెలుగు", "en-US": "English (US)", "es-ES": "Espanol (Espana)", "ja-JP": "日本語", "ko-KR": "한국어", "it-IT": "Italiano",
-    "fr-FR": "Français", "ms-MY": "Melayu", "ar-SA": "عربي", "id-ID": "Indonesia", "th-TH": "ไทย", "ru-RU": "Русский",
-    "vi-VN": "Vietnamese"
-};
+const locales = ["n/a", "hi-IN", "ta-IN", "pt-BR", "es-419", "de-DE", "te-IN", "en-US", "es-ES", "ja-JP", "ko-KR", "it-IT", "fr-FR", "ms-MY", "ar-SA", "id-ID", "th-TH", "ru-RU", "vi-VN"];
+const translations = { "n/a": "All", "hi-IN": "हिंदी", "ta-IN": "தமிழ்", "pt-BR": "Português (Brasil)", "es-419": "Espanol (América Latina)", "de-DE": "Deutsch", "te-IN": "తెలుగు", "en-US": "English (US)", "es-ES": "Espanol (Espana)", "ja-JP": "日本語", "ko-KR": "한국어", "it-IT": "Italiano", "fr-FR": "Français", "ms-MY": "Melayu", "ar-SA": "عربي", "id-ID": "Indonesia", "th-TH": "ไทย", "ru-RU": "Русский", "vi-VN": "Vietnamese" };
 const rtlLocales = ["ar-SA"];
 
-// Retrieve configuration
+// Variable declaration
+let allOverlays = [];
+let allAnimes = [];
+let animesWithLocale = [];
+let observerContent = null;
+
+// Configuration recovery
 const config = getConfig();
 console.info('Crunchyroll Mod Menu locale: ' + translations[config.locale]);
 
-// Array to store all fetched animes
-var allAnimes = [];
-
-// Array to store animes with the specified locale
-var animesWithLocale = [];
-
-// Target node to observe for changes
-const targetNode = document.getElementById("content");
-const observer = new MutationObserver(function (mutationList, observer) {
-    for (const mutation of mutationList) {
-        if (mutation.type === "childList" && mutation.addedNodes.length > 0) {
-            for (const node of mutation.addedNodes) {
-                if (node.nodeName === "DIV" && node.className.includes('erc-browse-collection')) {
-                    observeCollection(node);
-                    observer.disconnect();
-                }
-            }
-        }
-    }
-});
-observer.observe(targetNode, { attributes: false, childList: true, subtree: true });
 
 // Style for browse cards
 const style = document.createElement('style');
@@ -59,12 +36,11 @@ style.innerHTML = `
 
 .browse-card[data-translated="false"]:hover {
     opacity: ${opacityHoverNotTranslated} !important;
-}
-`;
+}`;
 
 document.head.appendChild(style);
 
-// Create the mod menu
+// Mod menu creation
 makeModMenu();
 document.addEventListener('keydown', function (event) {
     if (event.key === "Insert") {
@@ -73,11 +49,10 @@ document.addEventListener('keydown', function (event) {
     }
 });
 
-// Proxy XMLHttpRequest to filter animes with specified locale
+// XMLHttpRequest overload to filter animations by locale
 const originalXMLHttpRequest = window.XMLHttpRequest;
 window.XMLHttpRequest = () => { throw new Error('XMLHttpRequest is disabled.') };
 window.XMLHttpRequest = class XMLHttpRequestProxy extends originalXMLHttpRequest {
-
     filterResult = false;
 
     constructor() {
@@ -86,10 +61,7 @@ window.XMLHttpRequest = class XMLHttpRequestProxy extends originalXMLHttpRequest
     }
 
     open(method, url, async, user, password) {
-        if (
-            url.includes('content/v2/discover/browse?n=') ||
-            url.includes('content/v2/discover/browse?start=')
-        ) {
+        if (url.includes('content/v2/discover/browse?n=') || url.includes('content/v2/discover/browse?start=')) {
             this.filterResult = true;
         }
         return super.open(method, url, async, user, password);
@@ -98,84 +70,25 @@ window.XMLHttpRequest = class XMLHttpRequestProxy extends originalXMLHttpRequest
     parseResponse() {
         if (this.filterResult) {
             this.filterAnime();
+            setTimeout(parseAnimes, 1000);
         }
     }
 
     filterAnime() {
-        if (!this.filterResult) {
-            return;
-        }
+        if (!this.filterResult) return;
 
         const response = JSON.parse(this.responseText);
         if (config.locale === 'n/a') {
             setAnimes(response.data, response.data);
         } else {
-            const animes = response.data.filter(anime => {
-                return anime.series_metadata.audio_locales.includes(config.locale);
-            });
+            const animes = response.data.filter(anime => anime.series_metadata.audio_locales.includes(config.locale));
             setAnimes(response.data, animes);
         }
         this.filterResult = false;
     }
 }
 
-// Function to update the arrays of animes
-function setAnimes(newAnimes, newAnimesWithLocale) {
-    // Concatenate the new animes to the existing arrays
-    allAnimes = allAnimes.concat(newAnimes);
-    animesWithLocale = animesWithLocale.concat(newAnimesWithLocale);
-
-    // Add the IDs of new animes with the specified locale to the array
-    for (const anime of newAnimesWithLocale) {
-        animesWithLocale.push(anime.id);
-    }
-    // Remove duplicates from the array of animes with specified locale
-    animesWithLocale = Array.from(new Set(animesWithLocale));
-}
-
-// Function to fetch the browse card of an anime
-function fetchBrowseCard(anime) {
-    const foundNode = document.getElementById('anime-' + anime.id);
-    if (!foundNode) {
-        const link = anime.linked_resource_key.replace('cms:', '') + '/' + anime.slug_title;
-        let node = document.querySelector(`a[href$="${link}"]`);
-        if (!node) {
-            throw new Error('Node not found');
-        }
-
-        while (node.className !== 'browse-card') {
-            node = node.parentNode;
-        }
-        return node;
-    }
-    return foundNode;
-}
-
-// Function to observe changes in the collection of browse cards
-function observeCollection(collectionNode) {
-    const callback = function (mutationList, observer) {
-        for (const anime of allAnimes) {
-            const node = fetchBrowseCard(anime);
-            if (node && !node.getAttribute('id')) {
-                node.setAttribute('id', 'anime-' + anime.id);
-            }
-
-            if (animesWithLocale.includes(anime.id)) {
-                node.setAttribute('data-translated', 'true');
-            } else {
-                node.setAttribute('data-translated', 'false');
-                if (config.hiddenNotTranslated) {
-                    addOverlay(node);
-                }
-            }
-        }
-    }
-
-    const observer = new MutationObserver(callback);
-    observer.observe(collectionNode, { attributes: false, childList: true, subtree: true });
-}
-
-// Function to retrieve configuration from sessionStorage
+// Configuration recovery
 function getConfig() {
     const config = sessionStorage.getItem('config');
     if (config) {
@@ -188,12 +101,69 @@ function getConfig() {
     };
 }
 
-// Function to store configuration in sessionStorage
+// Save configuration
 function setConfig(config) {
     sessionStorage.setItem('config', JSON.stringify(config));
 }
 
-// Function to create the mod menu
+
+// Updating anime tables
+function setAnimes(newAnimes, newAnimesWithLocale) {
+    allAnimes = allAnimes.concat(newAnimes);
+    animesWithLocale = animesWithLocale.concat(newAnimesWithLocale);
+
+    for (const anime of newAnimesWithLocale) {
+        animesWithLocale.push(anime.id);
+    }
+    animesWithLocale = Array.from(new Set(animesWithLocale));
+}
+
+// Find node in collection
+function fetchBrowseCard(anime) {
+    const foundNode = document.getElementById('anime-' + anime.id);
+    if (!foundNode) {
+        const link = anime.linked_resource_key.replace('cms:', '') + '/' + anime.slug_title;
+        let node = document.querySelector(`a[href$="${link}"]`);
+        if (!node) {
+            throw new Error('Node not found');
+        }
+
+        while (node.className !== 'browse-card') {
+            node = node.parentNode;
+            if (!node) {
+                throw new Error('Node not found');
+            }
+        }
+        return node;
+    }
+    return foundNode;
+}
+
+function parseAnimes() {
+    for (const anime of allAnimes) {
+        const node = fetchBrowseCard(anime);
+        if (node && !node.getAttribute('id')) {
+            node.setAttribute('id', 'anime-' + anime.id);
+        }
+
+        if (animesWithLocale.includes(anime.id)) {
+            node.setAttribute('data-translated', 'true');
+        } else {
+            node.setAttribute('data-translated', 'false');
+            if (config.hiddenNotTranslated) {
+                addOverlay(node);
+            }
+        }
+    }
+}
+
+function observeCollection(collectionNode) {
+    const observer = new MutationObserver(function (mutationList, observer) {
+        parseAnimes();
+    });
+    observer.observe(collectionNode, { attributes: false, childList: true, subtree: true });
+}
+
 function makeModMenu() {
 
     const div = document.createElement('div');
@@ -235,13 +205,13 @@ function makeModMenu() {
 
     const legend = document.createElement('legend');
     legend.innerHTML = "<strong>Parameters :</strong>";
-    
+
     const div4 = document.createElement('div');
 
     const selectLable = document.createElement('label');
     selectLable.setAttribute("for", "locale-select");
     selectLable.innerHTML = "<strong>Select language :</strong>";
-    
+
     const select = document.createElement('select');
     select.setAttribute('id', 'locale-select');
     select.setAttribute('name', 'locale-select');
@@ -260,7 +230,7 @@ function makeModMenu() {
         }
         select.appendChild(option);
     }
-    
+
     const div5 = document.createElement('div');
 
     const checkLable = document.createElement('label');
@@ -273,12 +243,12 @@ function makeModMenu() {
     check.setAttribute('type', 'checkbox');
     check.style.margin = '15px auto';
     check.checked = config.hiddenNotTranslated ? true : false;
-    
+
     const div6 = document.createElement('div');
     div6.style.display = 'flex';
     div6.style.justifyContent = 'end';
     div6.style.marginBottom = '20px';
-    
+
     const button = document.createElement('button');
     button.type = 'button';
     button.innerText = 'Apply';
@@ -295,6 +265,7 @@ function makeModMenu() {
             locale: selectedLocale,
             hiddenNotTranslated: check.checked,
         });
+
         window.location.reload();
     })
 
@@ -305,7 +276,7 @@ function makeModMenu() {
     div5.appendChild(check);
 
     div6.appendChild(button);
-    
+
     figure.appendChild(legend);
     figure.appendChild(div4);
     figure.appendChild(div5);
@@ -330,7 +301,6 @@ function makeModMenu() {
     document.body.appendChild(div);
 }
 
-// Function to switch the display of the mod menu
 function switchDisplayModMenu() {
     const div = document.getElementById('popup1');
 
@@ -346,6 +316,7 @@ function switchDisplayModMenu() {
 
     const selectLocale = document.getElementById('locale-select');
     selectLocale.value = config.locale;
+
     const check = document.getElementById('hidden-not-translated');
     check.checked = config.hiddenNotTranslated ? true : false;
 }
@@ -364,4 +335,72 @@ function addOverlay(node) {
     img.src = `https://placehold.co/${node.offsetWidth}x${node.offsetHeight}/000000/FFF?text=Not+Translated`;
 
     document.body.appendChild(img);
+
+    allOverlays.push(img);
+}
+
+function buildEnvironment() {
+    const collectionNode = document.querySelector('div.erc-browse-collection');
+    if (collectionNode) {
+        observeCollection(collectionNode);
+        return true;
+    }
+
+    const targetNode = document.getElementById("content");
+    const callback = function (mutationList, observer) {
+        for (const mutation of mutationList) {
+            if (mutation.type === "childList" && mutation.addedNodes.length > 0) {
+                for (const node of mutation.addedNodes) {
+                    if (node.nodeName === "DIV" && node.className.includes('erc-browse-collection')) {
+                        observeCollection(node);
+                        observer.disconnect();
+                    }
+                }
+            }
+        }
+    };
+    observerContent = new MutationObserver(callback);
+    observerContent.observe(targetNode, { attributes: false, childList: true, subtree: true });
+}
+
+// Overloading history.pushState to detect URL changes
+const originalPushState = history.pushState;
+history.pushState = function (state) {
+    const result = originalPushState.apply(history, arguments);
+    if (typeof history.onpushstate === 'function') {
+        history.onpushstate({ state: state });
+    }
+    return result;
+};
+
+history.onpushstate = function (event) {
+    const destinationURL = document.location.href;
+    if (destinationURL.includes('/videos/popular')) {
+        buildEnvironment();
+    } else {
+        allAnimes = [];
+        animesWithLocale = [];
+
+        for (const overlay of allOverlays) {
+            document.body.removeChild(overlay);
+        }
+        if (observerContent) {
+            observerContent.disconnect();
+        }
+    }
+};
+
+// Initial construction of the environment
+if (window.location.href.includes('/videos/popular')) {
+    buildEnvironment();
+} else {
+    allAnimes = [];
+    animesWithLocale = [];
+
+    for (const overlay of allOverlays) {
+        document.body.removeChild(overlay);
+    }
+    if (observerContent) {
+        observerContent.disconnect();
+    }
 }
